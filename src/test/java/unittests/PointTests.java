2b88ec47127d29fd9dec261e371bda2f262db3be
ben18mk <benchik2004@gmail.com>
2023-03-21 15:48:49 +0200

    .gitignore added

---
+++ src/test/java/unittests/PointTests.java
@@ -0,0 +1,24 @@
+package unittests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTests {
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void distanceSquared() {
+    }
+
+    @Test
+    void distance() {
+    }
+}
